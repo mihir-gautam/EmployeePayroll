cba2fb7c8a3ca751a5cc5f583b9538ec897482b1
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-01 22:19:33 +0530

    [Mihir] Add. query for inserting data into table

---
+++ UC3_InsertDataIntoTable.sql
@@ -0,0 +1,7 @@
+--UC3 Ability to insert data into table
+insert into employee_payroll values
+('Bill',100000.00,'2018-01-03'),
+('Terisa',200000.00,'2019-11-13'),
+('Charlie',300000.00,'2020-05-21')
+
+select * from employee_payroll
