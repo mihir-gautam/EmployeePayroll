81a65356f0aaa5f263b105c054be14eebce8c02c
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-02 20:44:40 +0530

    [Mihir] Add. Query to break salary into its components

---
+++ UC9_ExtendSalaryToItsComponents.sql
@@ -0,0 +1,13 @@
+--UC9 Query to show salary in its components
+use payroll_service
+alter table employee_payroll
+add basic_pay money default 40000,
+add taxable_pay money default 0,
+add income_tax money default 0
+
+insert into employee_payroll (name, net_pay, start) values
+('Sam',20000.00,'2018-04-07');
+insert into employee_payroll(name, net_pay, gender, start) values
+('Naman',250000.00,'M','2019-10-10')
+
+select * from employee_payroll;
