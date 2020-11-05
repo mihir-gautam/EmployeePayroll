9a644b1d55fc815bcbec58c493e83703b97f8cc5
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-05 22:12:58 +0530

    [Mihir] Add. C# code to add and update data from database

---
+++ EmployeePayroll/EmployeeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayroll
+{
+    class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public char Gender { get; set; }
+        public decimal BasicPay { get; set; }
+        public int Deductions { get; set; }
+        public int TaxablePay { get; set; }
+        public int Tax { get; set; }
+        public double NetPay { get; set; }
+        public DateTime StartDate { get; set; }
+        public string City { get; set; }
+        public double Country { get; set; }
+
+    }
+
+
+}
---
+++ EmployeePayroll/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmployeePayroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+            employee.EmployeeName = "Indal";
+            employee.Department = "Tech";
+            employee.PhoneNumber = "6302907918";
+            employee.Address = "02-Khajauli";
+            employee.Gender = 'M';
+            employee.BasicPay = 10000.00M;
+            employee.Deductions = 1500;
+            employee.StartDate = employee.StartDate = Convert.ToDateTime("2020-11-03");
+            if (employeeRepo.AddEmployee(employee))
+                Console.WriteLine("Records added successfully");
+
+            employeeRepo.GetAllEmployee();
+        }
+    }
+}
